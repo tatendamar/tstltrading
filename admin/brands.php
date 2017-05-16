095c11bfc096913e9fb52b87b4b4681d7436f34b
Tatenda Marufu <tatendamarufu05@gmail.com>
2017-05-16 17:04:12 +0200

    config file

--- admin/brands.php
+++ admin/brands.php
@@ -5,8 +5,41 @@
  //brands from database
  $sql = "SELECT * FROM brand ORDER BY brand_brand";
  $results = $db->query($sql);
+ $errors = array();
+ //if add form submitted
+ if(isset($_POST['add_submit'])){
+   $brand = $_POST ['brand'];
+   //check if brand is blank
+    if($_POST['brand'] == ''){
+      $errors[] .= 'You must enter a brand!';
+    }
+    //check for brand existance in Database
+    $sql ="SELECT * FROM brand WHERE brand_brand = '$brand'";
+    $result = $db->query($sql);
+    $count = mysqli_num_rows($result);
+    if($count > 0){
+      $errors[] .= 'That brand exists';
+    }
+    //display errors
+    if(!empty($errors)){
+      echo display_errors($errors);
+    }else{
+      //add brand
+    }
+ }
  ?>
-<h2 class="text-center">brands</h2>
+<h2 class="text-center">brands</h2><hr>
+  <!-- form-->
+  <div class="text-center">
+    <form class="form-inline" action="brands.php" method="post">
+      <div class="form-group">
+        <label for="brand">Add A Product</label>
+        <input type="text" name="brand" id="brand" class="form-control" value="<?php echo((isset($_POST['brand']))?$_POST['brand']: ''); ?>">
+        <input type="submit" name="add_submit" value="Add Brand" class="btn btn-success">
+      </div>
+    </form>
+  </div><hr>
+
 <table class="table table-bordered table-striped table-auto">
   <thead>
     <th></th><th>Brand</th><th></th>
--- core/init.php
+++ core/init.php
@@ -4,5 +4,5 @@
     echo 'Database connection failed with following errors: '. mysqli_connect_error();
     die();
   }
-
-  define('BASEURL', '/series2/');
+ require_once '../config.php';
+ require_once BASEURL.'helpers/helpers.php';
